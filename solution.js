382b78af2ea82ebcf90d0c871a0e6baf22ce0bbb
Cem <cemkgrbz@gmail.com>
2022-07-05 13:36:33 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,15 @@
+//1
+
+const introSentence = 'Last year was ';
+const year = 2021;
+console.log(introSentence + year);
+
+// + concatenate string and number 
+
+//2
+
+const integer = '1005'
+console.log(parseInt(integer))
+
+const float = '10.05'
+console.log(parseFloat(float))
